90cef0227717868d026a01b2c27092254fd37410
Zach <zach.Brewer.1995@gmail.com>
2024-01-11 16:03:12 -0500

    added license badge and link generator functions

--- index.js
+++ index.js
@@ -3,79 +3,97 @@
 // TODO: Create a function to write README file
 // TODO: Create a function to initialize app
 
-const fs = require('fs');
-const inquirer = require('inquirer');
+const fs = require("fs");
+const inquirer = require("inquirer");
+const markdown = require('./utils/generateMarkdown');
 
 const prompt = inquirer.createPromptModule();
 
-prompt ([
+prompt([
   {
-    type: 'input',
-    message: 'What is the title of your project',
-    name: 'title',
-    default: 'README Generator',
+    type: "input",
+    message: "What is the title of your project",
+    name: "title",
+    default: "README Generator",
   },
   {
-    type: 'input',
-    message: 'Enter a description for your project',
-    name: 'description',
-    default: 'This project uses inquirer to promt the user with questions in the terminal that will generate a high-quality README',
+    type: "input",
+    message: "Enter a description for your project",
+    name: "description",
+    default:
+      "This project uses inquirer to promt the user with questions in the terminal that will generate a high-quality README",
   },
   {
-    type: 'input',
-    message: 'Describe the installation instructions of your project',
-    name: 'installation',
-    default: 'Run node index.js in the terminal to be prompted by the program and fill out each question as desired',
+    type: "input",
+    message: "Describe the installation instructions of your project",
+    name: "installation",
+    default:
+      "Run node index.js in the terminal to be prompted by the program and fill out each question as desired",
   },
   {
-    type: 'input',
-    message: 'Enter a usage for your application',
-    name: 'usage',
-    default: 'With this application users can fill out pre-determined fields via promtps in the terminal. The application will use the users inputs to generate a professional README',
+    type: "input",
+    message: "Enter a usage for your application",
+    name: "usage",
+    default:
+      "With this application users can fill out pre-determined fields via promtps in the terminal. The application will use the users inputs to generate a professional README",
   },
   {
-    type: 'input',
-    message: 'List any contributors of your project',
-    name: 'contributing',
-    default: 'N/A',
+    type: "input",
+    message: "List any contributors of your project",
+    name: "contributing",
+    default: "N/A",
   },
   {
-    type: 'input',
-    message: 'Enter your GitHub URL',
-    name: 'github',
-    default: 'https://github.com/ZacharyDOTpy',
+    type: "input",
+    message: "Enter your GitHub URL",
+    name: "github",
+    default: "https://github.com/ZacharyDOTpy",
   },
   {
-    type: 'input',
-    message: 'Enter your Email',
-    name: 'email',
-    default: 'myEmail@something.com',
+    type: "input",
+    message: "Enter your Email",
+    name: "email",
+    default: "myEmail@something.com",
+  },
+  {
+    type: "rawlist",
+    message: "Choose your license",
+    name: ["MIT", "Apache 2.0"],
+    default: "myEmail@something.com",
+  },
+]).then((data) => {
+  console.log(data);
+  const readMe = `# ${data.title}
+
+## License
+
+## Table of Contents
+
+- [Description]{#description}
+- [Installation]{#installation}
+- [Usage]{#usage}
+- [Contributing]{#contributing}
+- [Questions]{#questions}
+
+## Description
+  ${data.description}
+
+## Installation
+  ${data.installation}
+
+## Usage
+  ${data.usage}
+
+## Contributing
+  ${data.contributing}
+
+## Questions
+  ${data.github}
+  ${data.email}`;
+
+  if (!fs.existsSync("./output")) {
+    fs.mkdirSync("./output");
   }
-])
-  .then((data) => {
-    console.log(data);
-    const readMe = `# ${data.title}
-    ## License
-    
-    ## Description
-      ${data.description}
-    
-    ## Installation
-      ${data.installation}
-    
-    ## Usage
-      ${data.usage}
-    
-    ## Contributing
-      ${data.contributing}
 
-    ## Questions
-      ${data.github}
-      ${data.email}`
-
-    if (!fs.existsSync('./output')) {
-      fs.mkdirSync('./output');
-    }
-
-    fs.writeFileSync('./output/README.md', readMe);
-  })
+  fs.writeFileSync("./output/README.md", readMe);
+});
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,72 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// generates license badge
+function renderLicenseBadge(license) {
+  if(license === 'MIT') {
+    return `<img src="https://img.shields.io/badge/License-${license}-blue" />`; 
+} else if (license === 'BSD'){
+    return `<img src="https://img.shields.io/badge/License-${license}-blue" />`; 
+} else {
+    return '';
+}  
+};
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+// generates license link
+function renderLicenseLink(license) {
+  if(license === 'MIT') {
+      return "Click this link for more information about your license: " + `(https://opensource.org/licenses/${license})`; 
+  } else if (license === 'BSD'){
+      return "Click this link for more information about your license: " + `(https://opensource.org/licenses/${license})`; 
+  } else {
+      return '';
+  }  
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+};
 
 // TODO: Create a function to generate markdown for README
+// README template
 function generateMarkdown(data) {
-  return `# ${data.title}
 
-`;
-}
+  return `
+# ${data.title}
+  
+- ${renderLicenseBadge(data.license)}
+
+## Description
+  
+-${data.description}
+
+## Table of Contents
+
+- [Installation]{#installation}
+- [Usage]{#usage}
+- [Contributing]{#contributing}
+- [Questions]{#questions}
+
+## Installation
+  ${data.installation}
+
+## Usage
+  ${data.usage}
+
+## Contributing
+  ${data.contributing}
+
+## License
+
+  - ${renderLicenseBadge(data.license)}
+
+  - ${renderLicenseLink(data.license)}
+
+## Questions
+  ${data.github}
+  ${data.email}`;
+  
+  
+      
+  
+  }
 
 module.exports = generateMarkdown;
